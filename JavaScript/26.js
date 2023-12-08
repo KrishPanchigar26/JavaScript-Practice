8c9ace299c4fc1fc34f130c82e8da9cc644eb2a9
KrishPanchigar26 <151987264+KrishPanchigar26@users.noreply.github.com>
2023-12-08 16:04:01 +0530

    Add files via upload

---
+++ JavaScript/26.js
@@ -0,0 +1,28 @@
+i = 0
+
+while (i <= 20) {
+    console.log(`value of i is ${i}`);
+    i = i + 2;
+}
+
+myArray = ["Ironman", "Thor", "Hulk"]
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
+
+let score1 = 11
+
+do {
+    console.log(`Score is ${score1}`);
+    score1++;
+} while (score1 <= 10);
